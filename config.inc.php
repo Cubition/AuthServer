56e06f83d77f524ba6f0f6787fab6e55a12236b9
Xesau <ck-duinrand@zeelandnet.nl>
2015-01-22 19:11:16 +0100

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Cubition PHP Authentication API
+ * Released under the MIT Licence
+ * Copyright 2015 - Cubition Team
+ */
+
+$mysql_host     = 'localhost';
+$mysql_username = 'root';
+$mysql_password = 'password';
+$mysql_database = 'cubition_auth';
+
+$mysql_table_prefix = 'cub_';
+
+$auto_registration = true;
