561d7ac179b1b108dcf5b8b8b1deae1901e2b637
Xesau <Xesau@users.noreply.github.com>
2015-04-02 20:56:19 +0200

    @jselby Why on earth move this file?

--- config.php
+++ config.php
@@ -9,6 +9,6 @@
 
 // # MySQL Configuration
 $mysqlHost = '127.0.0.1';
-$mysqlUser = 'cubition';
-$mysqlPass = 'cubition123';
-$mysqlDB = 'cubition';
+$mysqlUser = 'root';
+$mysqlPass = 'password';
+$mysqlDB = 'auth_server';
